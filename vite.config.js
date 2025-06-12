3b0b44a17a151f970952c994bd056d1158446d5b
eacollectibles <ea.collectibles.service@gmail.com>
2025-06-12 12:36:25 -0700

    Update vite.config.js
    Fix Vite base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // 👈 ensures correct asset paths on Netlify
   plugins: [react()],
 })
